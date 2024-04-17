87154dd3b7e66c804a76bbc4c885aba082d9fe09
lisaklein <lisa@q42.nl>
2024-04-17 15:57:28 +0200

    Fix language toggle

---
+++ src/theme/NavbarItem/LocaleDropdownNavbarItem/index.tsx
@@ -0,0 +1,62 @@
+import React, { MouseEventHandler } from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { translate } from '@docusaurus/Translate';
+import { useLocation } from '@docusaurus/router';
+import DropdownNavbarItem from '@theme/NavbarItem/DropdownNavbarItem';
+import type { LinkLikeNavbarItemProps } from '@theme/NavbarItem';
+import type { Props } from '@theme/NavbarItem/LocaleDropdownNavbarItem';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
+export default function LocaleDropdownNavbarItem({
+  mobile,
+  dropdownItemsBefore,
+  dropdownItemsAfter,
+  queryString = '',
+  ...props
+}: Props): JSX.Element {
+  const {
+    i18n: { currentLocale, locales, localeConfigs },
+  } = useDocusaurusContext();
+  const { search, hash } = useLocation();
+
+  // Mobile is handled a bit differently
+  const dropdownLabel = mobile
+    ? translate({
+        message: 'Languages',
+        id: 'theme.navbar.mobileLanguageDropdown.label',
+        description: 'The label for the mobile language switcher dropdown',
+      })
+    : localeConfigs[currentLocale]!.label;
+
+  return (
+    <BrowserOnly>
+      {() => {
+        const localeItems = locales.map((locale): LinkLikeNavbarItemProps => {
+          // preserve ?search#hash suffix on locale switches
+          const to = `${window.location.origin}/${locale}${search}${hash}${queryString}`;
+          return {
+            label: localeConfigs[locale]!.label,
+            lang: localeConfigs[locale]!.htmlLang,
+            to,
+            target: '_self',
+            autoAddBaseUrl: false,
+            className:
+              // eslint-disable-next-line no-nested-ternary
+              locale === currentLocale
+                ? // Similar idea as DefaultNavbarItem: select the right Infima active
+                  // class name. This cannot be substituted with isActive, because the
+                  // target URLs contain `pathname://` and therefore are not NavLinks!
+                  mobile
+                  ? 'menu__link--active'
+                  : 'dropdown__link--active'
+                : '',
+          };
+        });
+
+        const items = [...dropdownItemsBefore, ...localeItems, ...dropdownItemsAfter];
+
+        return <DropdownNavbarItem {...props} mobile={mobile} label={dropdownLabel} items={items} />;
+      }}
+    </BrowserOnly>
+  );
+}
