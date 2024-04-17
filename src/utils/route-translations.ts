26097afde33add7fb5919a10da7fb9d31a7dd86e
lisaklein <lisa@q42.nl>
2024-04-17 15:29:12 +0200

    Remove console log

--- src/utils/route-translations.ts
+++ src/utils/route-translations.ts
@@ -6,8 +6,6 @@ export const getTranslatedPath = (path: string, capitalized?: true) => {
     stats: 'stats',
   };
 
-  console.log(process.env.DOCUSAURUS_CURRENT_LOCALE);
-
   const isNl = process.env.DOCUSAURUS_CURRENT_LOCALE === 'nl';
   const translatedPath = isNl ? dutchTranslations[path] : path;
   return `${capitalized ? translatedPath.charAt(0).toUpperCase() + translatedPath.slice(1) : translatedPath}`;
