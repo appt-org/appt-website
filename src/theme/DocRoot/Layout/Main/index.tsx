d35412817b119ee68b23f67e91b7290af09848e4
lisaklein <lisa@q42.nl>
2024-04-19 11:49:57 +0200

    Fix layout

---
+++ src/theme/DocRoot/Layout/Main/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import type { Props } from '@theme/DocRoot/Layout/Main';
+
+export default function DocRootLayoutMain({ children }: Props): JSX.Element {
+  return (
+    <main className="flex-auto py-6 sm:py-12 overflow-hidden">
+      <div>{children}</div>
+    </main>
+  );
+}
