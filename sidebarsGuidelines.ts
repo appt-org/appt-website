3284c02805f083247a47c7959ca449b10d80b10f
lisaklein <lisa@q42.nl>
2024-04-26 15:49:29 +0200

    Fix labels

--- sidebarsGuidelines.ts
+++ sidebarsGuidelines.ts
@@ -69,6 +69,122 @@ const labels = {
     en: '4.1 Compatible',
     nl: '4.1 Compatibel',
   },
+  '1.1.1': {
+    en: '1.1.1 Non-text Content',
+    nl: '1.1.1 Niet-tekstinhoud',
+  },
+  '1.2.1': {
+    en: '1.2.1 Audio-only and Video-only (Prerecorded)',
+    nl: '1.2.1 Alleen audio en alleen video (vooraf opgenomen)',
+  },
+  '1.2.2': {
+    en: '1.2.2 Captions (Prerecorded)',
+    nl: '1.2.2 Ondertiteling (vooraf opgenomen)',
+  },
+  '1.2.3': {
+    en: '1.2.3 Audio Description or Media Alternative (Prerecorded)',
+    nl: '1.2.3 Audiobeschrijving of media-alternatief (vooraf opgenomen)',
+  },
+  '1.2.4': {
+    en: '1.2.4 Captions (Live)',
+    nl: '1.2.4 Ondertiteling (live)',
+  },
+  '1.2.5': {
+    en: '1.2.5 Audio Description (Prerecorded)',
+    nl: '1.2.5 Audiobeschrijving (vooraf opgenomen)',
+  },
+  '1.2.6': {
+    en: '1.2.6 Sign Language (Prerecorded)',
+    nl: '1.2.6 Gebarentaal (vooraf opgenomen)',
+  },
+  '1.2.7': {
+    en: '1.2.7 Extended Audio Description (Prerecorded)',
+    nl: '1.2.7 Uitgebreide audiobeschrijving (vooraf opgenomen)',
+  },
+  '1.2.8': {
+    en: '1.2.8 Media Alternative (Prerecorded)',
+    nl: '1.2.8 Media-alternatief (vooraf opgenomen)',
+  },
+  '1.2.9': {
+    en: '1.2.9 Audio-only (Live)',
+    nl: '1.2.9 Alleen audio (live)',
+  },
+  '1.3.1': {
+    en: '1.3.1 Info and Relationships',
+    nl: '1.3.1 Informatie en relaties',
+  },
+  '1.3.2': {
+    en: '1.3.2 Meaningful Sequence',
+    nl: '1.3.2 Betekenisvolle volgorde',
+  },
+  '1.3.3': {
+    en: '1.3.3 Sensory Characteristics',
+    nl: '1.3.3 Zintuiglijke kenmerken',
+  },
+  '1.3.4': {
+    en: '1.3.4 Orientation',
+    nl: '1.3.4 Oriëntatie',
+  },
+  '1.3.5': {
+    en: '1.3.5 Audio-only and Video-only (Prerecorded)',
+    nl: '1.3.5 Alleen audio en alleen video (vooraf opgenomen)',
+  },
+  '1.3.6': {
+    en: '1.3.6 Captions (Prerecorded)',
+    nl: '1.3.6 Ondertiteling (vooraf opgenomen)',
+  },
+  '1.4.1': {
+    en: '1.4.1 Audio Description or Media Alternative (Prerecorded)',
+    nl: '1.4.1 Audiobeschrijving of media-alternatief (vooraf opgenomen)',
+  },
+  '1.4.2': {
+    en: '1.4.2 Captions (Prerecorded)',
+    nl: '1.4.2 Ondertiteling (vooraf opgenomen)',
+  },
+  '1.4.3': {
+    en: '1.4.3 Audio Description or Media Alternative (Prerecorded)',
+    nl: '1.4.3 Audiobeschrijving of media-alternatief (vooraf opgenomen)',
+  },
+  '1.4.4': {
+    en: '1.4.4 Captions (Live)',
+    nl: '1.4.4 Ondertiteling (live)',
+  },
+  '1.4.5': {
+    en: '1.4.5 Audio Description (Prerecorded)',
+    nl: '1.4.5 Audiobeschrijving (vooraf opgenomen)',
+  },
+  '1.4.6': {
+    en: '1.4.6 Sign Language (Prerecorded)',
+    nl: '1.4.6 Gebarentaal (vooraf opgenomen)',
+  },
+  '1.4.7': {
+    en: '1.4.7 Extended Audio Description (Prerecorded)',
+    nl: '1.4.7 Uitgebreide audiobeschrijving (vooraf opgenomen)',
+  },
+  '1.4.8': {
+    en: '1.4.8 Media Alternative (Prerecorded)',
+    nl: '1.4.8 Media-alternatief (vooraf opgenomen)',
+  },
+  '1.4.9': {
+    en: '1.4.9 Audio-only (Live)',
+    nl: '1.4.9 Alleen audio (live)',
+  },
+  '1.4.10': {
+    en: '1.4.10 Info and Relationships',
+    nl: '1.4.10 Informatie en relaties',
+  },
+  '1.4.11': {
+    en: '1.4.11 Meaningful Sequence',
+    nl: '1.4.11 Betekenisvolle volgorde',
+  },
+  '1.4.12': {
+    en: '1.4.12 Sensory Characteristics',
+    nl: '1.4.12 Zintuiglijke kenmerken',
+  },
+  '1.4.13': {
+    en: '1.4.13 Orientation',
+    nl: '1.4.13 Oriëntatie',
+  },
 };
 
 const getLabel = (key: string) => {
@@ -101,148 +217,148 @@ const sidebars: SidebarsConfig = {
           items: [
             {
               type: 'doc',
-              label: '1.1.1 Non-text Content',
+              label: getLabel('1.1.1'),
               id: 'wcag/success-criterion-1-1-1/index',
             },
 
             {
               type: 'doc',
-              label: '1.2.1 Audio-only and Video-only (Prerecorded)',
+              label: getLabel('1.2.1'),
               id: 'wcag/success-criterion-1-2-1/index',
             },
             {
               type: 'doc',
-              label: '1.2.2 Captions (Prerecorded)',
+              label: getLabel('1.2.2'),
               id: 'wcag/success-criterion-1-2-2/index',
             },
             {
               type: 'doc',
-              label: '1.2.3 Audio Description or Media Alternative (Prerecorded)',
+              label: getLabel('1.2.3'),
               id: 'wcag/success-criterion-1-2-3/index',
             },
             {
               type: 'doc',
-              label: '1.2.4 Captions (Live)',
+              label: getLabel('1.2.4'),
               id: 'wcag/success-criterion-1-2-4/index',
             },
             {
               type: 'doc',
-              label: '1.2.5 Audio Description (Prerecorded)',
+              label: getLabel('1.2.5'),
               id: 'wcag/success-criterion-1-2-5/index',
             },
             {
               type: 'doc',
-              label: '1.2.6 Sign Language (Prerecorded)',
+              label: getLabel('1.2.6'),
               id: 'wcag/success-criterion-1-2-6/index',
             },
             {
               type: 'doc',
-              label: '1.2.7 Extended Audio Description (Prerecorded)',
+              label: getLabel('1.2.7'),
               id: 'wcag/success-criterion-1-2-7/index',
             },
             {
               type: 'doc',
-              label: '1.2.8 Media Alternative (Prerecorded)',
+              label: getLabel('1.2.8'),
               id: 'wcag/success-criterion-1-2-8/index',
             },
             {
               type: 'doc',
-              label: '1.2.9 Audio-only (Live)',
+              label: getLabel('1.2.9'),
               id: 'wcag/success-criterion-1-2-9/index',
             },
             {
               type: 'doc',
-              label: '1.3.1 Info and Relationships',
+              label: getLabel('1.3.1'),
               id: 'wcag/success-criterion-1-3-1/index',
             },
             {
               type: 'doc',
-              label: '1.3.2 Meaningful Sequence',
+              label: getLabel('1.3.2'),
               id: 'wcag/success-criterion-1-3-2/index',
             },
             {
               type: 'doc',
-              label: '1.3.3 Sensory Characteristics',
+              label: getLabel('1.3.3'),
               id: 'wcag/success-criterion-1-3-3/index',
             },
             {
               type: 'doc',
-              label: '1.3.4 Orientation',
+              label: getLabel('1.3.4'),
               id: 'wcag/success-criterion-1-3-4/index',
             },
             {
               type: 'doc',
-              label: '1.3.5 Audio-only and Video-only (Prerecorded)',
+              label: getLabel('1.3.5'),
               id: 'wcag/success-criterion-1-3-5/index',
             },
             {
               type: 'doc',
-              label: '1.3.6 Captions (Prerecorded)',
+              label: getLabel('1.3.6'),
               id: 'wcag/success-criterion-1-3-6/index',
             },
             {
               type: 'doc',
-              label: '1.4.1 Audio Description or Media Alternative (Prerecorded)',
+              label: getLabel('1.4.1'),
               id: 'wcag/success-criterion-1-4-1/index',
             },
             {
               type: 'doc',
-              label: '1.4.2 Captions (Prerecorded)',
+              label: getLabel('1.4.2'),
               id: 'wcag/success-criterion-1-4-2/index',
             },
             {
               type: 'doc',
-              label: '1.4.3 Audio Description or Media Alternative (Prerecorded)',
+              label: getLabel('1.4.3'),
               id: 'wcag/success-criterion-1-4-3/index',
             },
             {
               type: 'doc',
-              label: '1.4.4 Captions (Live)',
+              label: getLabel('1.4.4'),
               id: 'wcag/success-criterion-1-4-4/index',
             },
             {
               type: 'doc',
-              label: '1.4.5 Audio Description (Prerecorded)',
+              label: getLabel('1.4.5'),
               id: 'wcag/success-criterion-1-4-5/index',
             },
             {
               type: 'doc',
-              label: '1.4.6 Sign Language (Prerecorded)',
+              label: getLabel('1.4.6'),
               id: 'wcag/success-criterion-1-4-6/index',
             },
             {
               type: 'doc',
-              label: '1.4.7 Extended Audio Description (Prerecorded)',
+              label: getLabel('1.4.7'),
               id: 'wcag/success-criterion-1-4-7/index',
             },
             {
               type: 'doc',
-              label: '1.4.8 Media Alternative (Prerecorded)',
+              label: getLabel('1.4.8'),
               id: 'wcag/success-criterion-1-4-8/index',
             },
             {
               type: 'doc',
-              label: '1.4.9 Audio-only (Live)',
+              label: getLabel('1.4.9'),
               id: 'wcag/success-criterion-1-4-9/index',
             },
             {
               type: 'doc',
-              label: '1.4.10 Info and Relationships',
+              label: getLabel('1.4.10'),
               id: 'wcag/success-criterion-1-4-10/index',
             },
             {
               type: 'doc',
-              label: '1.4.11 Meaningful Sequence',
+              label: getLabel('1.4.11'),
               id: 'wcag/success-criterion-1-4-11/index',
             },
             {
               type: 'doc',
-              label: '1.4.12 Sensory Characteristics',
+              label: getLabel('1.4.12'),
               id: 'wcag/success-criterion-1-4-12/index',
             },
             {
               type: 'doc',
-              label: '1.4.13 Orientation',
+              label: getLabel('1.4.13'),
               id: 'wcag/success-criterion-1-4-13/index',
             },
           ],
