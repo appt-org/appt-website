32d25491950d87b05024bdf78ee43d2e097e9676
lisaklein <lisa@q42.nl>
2024-05-01 14:21:24 +0200

    Make hero able to handle multiple types of images, also on guideline pages

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -22,19 +22,20 @@ export type HeroProps = {
 
 export function Hero({ title, introText, prefix, suffix, imageSrc, imageDarkSrc, alt }: HeroProps) {
   const wrapperClasses = clsx('relative', { 'mb-10': imageSrc || introText });
-  const classes = clsx('flex flex-col items-center flex-1 md:flex-row', {
-    'justify-center': !!imageSrc,
-  });
-  const containerClasses = clsx('max-w-md mx-auto', { 'w-full': !imageSrc }, { 'py-8 sm:py-16': imageSrc });
+  const containerClasses = clsx(
+    'hero-container max-w-md mx-auto',
+    { 'w-full': !imageSrc },
+    { 'py-8 sm:py-16': imageSrc },
+  );
 
   const prefixSuffixText = prefix && suffix ? `${prefix} - ${suffix}` : prefix ? prefix : suffix ? suffix : undefined;
 
   return (
     <div className={wrapperClasses}>
       <div className={containerClasses}>
-        <div className={classes}>
+        <div className="flex flex-col flex-1 md:items-center  md:flex-row">
           {!!imageSrc && (
-            <div className="relative self-stretch grow-0 mb-8 mt-0 md:my-auto md:mr-8 md:h-auto md:basis-1/3 shrink-0">
+            <div className="relative self-stretch grow-0 mb-8 mt-0 md:my-auto md:mr-8 md:h-auto shrink-0">
               <Image src={imageSrc} dark={imageDarkSrc} alt={alt} />
             </div>
           )}
